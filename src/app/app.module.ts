77e83c06cc2a81bd33938634b3d5683abdc87326
Eduardo Esparza <lalopenaesparza@gmail.com>
2021-05-31 14:34:19 -0500

    navbar y home component
    Se modificaron las opciones del navbar. Se creó el componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
