a6a8f1fc970be0d6512ff905982df49406e6d392
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2021-06-10 14:24:09 -0500

    Screen reader FAQ

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../services/screenReader/global.service';
+
 
 @Component({
   selector: 'app-faq',
@@ -8,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  validarSpeak: GlobalService; 
+  mensaje: any;
+  oracion: any;
+
+  constructor(global: GlobalService) { 
+    if ('speechSynthesis' in window) {
+      this.mensaje = new SpeechSynthesisUtterance();
+    } else {
+      alert("Lo siento, tu navegador no soporta esta tecnología");
+    }
+    this.validarSpeak = global;
+    console.log(this.validarSpeak);
+  }
 
   ngOnInit(): void {
   }
+
+  playSpeak(f1:string) {
+  
+    //this.oracion = document.getElementById(t1).innerHTML +". " + document.getElementById(t2).innerHTML +". " + document.getElementById(t3).innerHTML;
+    this.oracion = document.getElementById(f1).innerHTML;
+    this.mensaje.text= this.oracion; 
+    if(speechSynthesis.paused){
+      speechSynthesis.resume();
+    }else{
+      speechSynthesis.cancel();
+      speechSynthesis.speak(this.mensaje);
+    }
+    
+  }
+
+  stopSpeak(){
+    speechSynthesis.pause();
+  }
+
+  playSpeak2(f2:string,f3:string) {
+  
+    this.oracion = document.getElementById(f2).innerHTML +". " + document.getElementById(f3).innerHTML;
+    this.mensaje.text= this.oracion; 
+    if(speechSynthesis.paused){
+      speechSynthesis.resume();
+    }else{
+      speechSynthesis.cancel();
+      speechSynthesis.speak(this.mensaje);
+    }
+  }
+
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import * as auth from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { AccountService } from '../services/login/account.service';
+import { GlobalService } from '../services/screenReader/global.service';
+
 declare var $: any;
 @Component({
   selector: 'app-navbar',
@@ -10,12 +12,23 @@ declare var $: any;
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+
+  validarSpeak: GlobalService; 
+
   constructor(
     public auth: AngularFireAuth,
     public accountService: AccountService,
-    public router: Router
-  ) {}
+    public router: Router,
+    private global: GlobalService
+  ){
+    this.validarSpeak=global;
+  }
 
   ngOnInit(): void {
   }
+
+  habilitarSpeak(){
+    this.global.band=!this.global.band; 
+  }
+
 }
