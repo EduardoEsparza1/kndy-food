660c9e5637ee10606c743923d63b1b15ca6ffc14
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2021-06-15 15:36:07 -0500

    Funcionando implementación de Node con Angular y Envio de correo
    Ahora si!!!, se implemento node al proyecto, esta en modo desarrollo, asi que para correr el proyecto, basta con dirigirse a la carpeta de my-app y correr el proyecto normalmente con "ng serve --o". Y para ver funcionar el envio de correo de Contacto, desde la carpeta raiz, nada mas ingresen el comando "npm start"

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express"); //importar express
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const configMensaje = require('./configMensaje');
+
+
+const app = express(); //crear al servidor
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post('/api/formulario', (req, res) => {
+    configMensaje(req.body);
+    res.status(200).send();
+})
+
+app.get('/', (req,res) => {
+    res.send('App Works !!!!');
+});
+
+
+app.listen(port, () => {
+    console.log(`hola servidor ejecucion en http://localhost:${port}`);
+})
