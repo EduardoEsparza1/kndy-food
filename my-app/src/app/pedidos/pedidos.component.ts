bcfd02073bf1100358e2b41214cdf828f8d5ae3e
moni12raid <moni12raid@gmail.com>
2021-06-15 20:08:58 -0500

    Revert "Merge branch 'master' of https://github.com/EduardoEsparza1/kndy-food"
    This reverts commit a7ee3567c687ebd52d7ab1a08675144c46eb756c, reversing
    changes made to e0be1d006bb984e449a74cd274ec167ece4b3bee.

--- my-app/src/app/pedidos/pedidos.component.ts
+++ my-app/src/app/pedidos/pedidos.component.ts
@@ -3,44 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { __awaiter } from 'tslib';
 import { FirestoreService } from '../services/firestore/firestore.service';
 import { AccountService } from '../services/login/account.service';
-import { CodigoqrService } from '../services/codigoQR/codigoqr.service';
 
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.component.html',
-  styleUrls: ['./pedidos.component.css'],
+  styleUrls: ['./pedidos.component.css']
 })
 export class PedidosComponent implements OnInit {
-  pedidos: any;
-  datospedidos: any;
+
+  pedidos: any
+
   constructor(
     private firestoreService: FirestoreService,
-    private accountService: AccountService,
-    private codigoqr: CodigoqrService
-  ) {}
+    private accountService: AccountService
+    ) { }
 
   ngOnInit(): void {
-    this.pedidos = [];
-    this.datospedidos = [];
+    
+    this.pedidos = []
+    
+    this.firestoreService.getPedidos(this.accountService.uid).subscribe(pedidoSnapshot => {
+      pedidoSnapshot.forEach(pedido => {
 
-    this.firestoreService
-      .getPedidos(this.accountService.uid)
-      .subscribe((pedidoSnapshot) => {
-        pedidoSnapshot.forEach((pedido) => {
-          let apartado = pedido.payload.doc.data();
+        let apartado = pedido.payload.doc.data()
 
-          let data = { fecha: apartado['fecha'], data: apartado['data'] };
+        let data = {fecha: apartado['fecha'], data: apartado['data']}
 
-          this.pedidos.push(data);
-          console.log(data);
-        });
-      });
+        this.pedidos.push(data)
+        console.log(data)
+      })
+    })
 
-    this.codigoqr.getDatosPedidos().subscribe(
-      (res) => {
-        this.datospedidos = res
-        console.log(res)},
-      (err) => console.log(err)
-    );
   }
+
 }
