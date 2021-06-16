b4d74f045abea80aaaabd163c109bdb2d20e4702
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2021-06-16 01:59:54 -0500

    Validación formulario de contacto

--- my-app/src/app/contact/contact.component.ts
+++ my-app/src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from '../services/message/message.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../services/login/account.service';
 
 
 import Swal from 'sweetalert2';
@@ -14,22 +15,30 @@ import Swal from 'sweetalert2';
 export class ContactComponent implements OnInit {
   @Input() action: string;
 
+  triedDatos: boolean
+
   constructor(
     public router: Router,
-    public _MessageService: MessageService
+    public _MessageService: MessageService,
+    public accountService: AccountService
   ) { }
 
-
-  public datos = new FormGroup({
+  public datosForm = new FormGroup({
     nombre: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
     mensaje: new FormControl('', Validators.required),
   });
 
-  contactForm(datos) {
-    this._MessageService.sendMessage(datos).subscribe(() => {
-      Swal.fire('Mensaje enviado correctamente')
-   });
+  
+  contactForm(datosForm) {
+   this.triedDatos = true
+    if(this.datosForm.valid) {
+      this._MessageService.sendMessage(datosForm).subscribe(() => {
+        Swal.fire('Mensaje enviado correctamente')
+      });
+      
+      this.triedDatos = false
+    }
   }
 
   ngOnInit() {
