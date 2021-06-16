926dc1e1f654e933c910542a23c36c0ba84baaca
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2021-06-16 01:23:07 -0500

    Cambio a Formulario Reactivo de Contacto

--- my-app/src/app/contact/contact.component.ts
+++ my-app/src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from '../services/message/message.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 import Swal from 'sweetalert2';
 
@@ -17,14 +19,20 @@ export class ContactComponent implements OnInit {
     public _MessageService: MessageService
   ) { }
 
-  contactForm(form) {
-    this._MessageService.sendMessage(form).subscribe(() => {
+
+  public datos = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required),
+  });
+
+  contactForm(datos) {
+    this._MessageService.sendMessage(datos).subscribe(() => {
       Swal.fire('Mensaje enviado correctamente')
    });
   }
 
   ngOnInit() {
-  
   }
 
 }
